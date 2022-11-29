5fe03ec99e9d4c237ccc172474689a24c729a3b6
Akhilesh <akhileshudayashankar@gmail.com>
2022-11-28 18:50:51 -0800

    Updated for openstack-4- AMI ID

--- auto-scale/instance_manager.py
+++ auto-scale/instance_manager.py
@@ -2,7 +2,7 @@ import boto3
 
 ec2_client = boto3.client('ec2', region_name='us-east-1')
 
-AMI_ID = "ami-019d98b59b144d4d3"
+AMI_ID = "ami-08b69d15e0b2039cc"
 ROLE_ARN = "arn:aws:iam::378107157540:instance-profile/sqs-s3-ec2-full-access"
 KEY_NAME = "KeyPair1"
 TAG_SPEC = [
